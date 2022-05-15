8e2a60b92234358e7208f15458a037e02f15d928
abdullah <abdullahsalameh66@gmail.com>
2022-05-15 16:44:00 +0300

    creating board component ✨

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from './Box';
+
+
+export const Board = ({board, onClick}) => {
+  return (
+    <div>
+        {board.map((value,index)=>(
+            <Box value={value} onClick={()=> onClick(index)} />
+        ))}
+        
+    </div>
+  )
+}
